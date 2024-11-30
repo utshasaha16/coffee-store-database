02441971aa2f92912bde3d0461bf7aeb26163f57
utshasaha16 <utsharoy12345@gmail.com>
2024-11-30 16:17:35 +0600

    new user add delete or get

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iwlha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,6 +26,8 @@ async function run() {
     await client.connect();
 
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
+
+    const userCollection = client.db("coffeeDB").collection("users");
 
     app.get("/coffee", async (req, res) => {
       const cursor = coffeeCollection.find();
@@ -63,15 +64,48 @@ async function run() {
           details: updatedCoffee.details,
           photoUrl: updatedCoffee.photoUrl,
         },
-      }
+      };
       const result = await coffeeCollection.updateOne(filter, coffee, options);
-      res.send(result)
+      res.send(result);
     });
 
     app.delete("/coffee/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // users related apis
+
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    app.patch("/users", async (req, res) => {
+      const email = req.body.email;
+      const filter = { email };
+      const updatedEmail = {
+        $set: {
+          lastSignInTime: req.body?.lastSignInTime
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedEmail);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     });
 
